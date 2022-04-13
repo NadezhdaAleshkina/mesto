335f022642b7f3d896335677507f2e40825e20fd
NadezhdaAleshkina <nadegdarum@mail.ru>
2022-04-13 21:12:26 +0300

    комментарий к коммиту

--- script/script.js
+++ script/script.js
@@ -4,30 +4,26 @@ const profileJob = document.querySelector(".profile__job");
 const nameInput = popup.querySelector(".popup__input_type_name");
 const jobInput = popup.querySelector(".popup__input_type_job");
 const openEditButton = document.querySelector(".profile__btn-edit");
-const popupSaveButton = popup.querySelector(".popup__btn-save");
+const popupSaveButton = popup.querySelector(".popup__form");
 const closeEditButton = popup.querySelector(".popup__btn-close");
-
-nameInput.value = profileName.textContent;
-jobInput.value = profileJob.textContent;
 
 function popupOpenToggle() {
   popup.classList.toggle("popup_opened");
 }
 
-function popupOverlayClic(evt) {
-  if (evt.target === evt.currentTarget) {
-    popupOpenToggle();
-  }
-}
 openEditButton.addEventListener("click", popupOpenToggle);
 closeEditButton.addEventListener("click", popupOpenToggle);
-popup.addEventListener("click", popupOverlayClic);
+
+function popupOpen() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+popupOpen();
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
 }
 
-popupSaveButton.addEventListener("click", formSubmitHandler);
+popupSaveButton.addEventListener("submit", formSubmitHandler);
